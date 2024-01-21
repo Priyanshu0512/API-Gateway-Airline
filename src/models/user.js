74c3e3ebb07953e1fcb8b423edfab853bf7de3b0
Priyanshu0512 <priyanshuk0595@gmail.com>
2024-01-21 15:43:08 +0530

    Added password encryption functionality.

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,9 @@
 const {
   Model
 } = require('sequelize');
+
+const bcrypt = require('bcrypt');
+const {ServerConfig} =require('../config')
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -32,6 +35,14 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+  });
+
+  User.beforeCreate(function encrypt(user){
+    console.log(user.password);
+    const encryptedPassword = bcrypt.hashSync(user.password,+ServerConfig.SALT_ROUNDS);
+    user.password=encryptedPassword;
+    console.log(user.password);
+
   });
   return User;
 };
